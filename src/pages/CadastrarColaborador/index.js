52ca9d714920e6625cf8cdc823a3ac2de448c325
Andre <andrelucasruizalmeida@gmail.com>
2022-09-27 22:30:42 -0300

    feat: esqueleto de formulário de cadastro de colaborador

--- src/pages/CadastrarColaborador/index.js
+++ src/pages/CadastrarColaborador/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 
 import Logo from '../../common/Logo';
 
@@ -12,7 +12,34 @@ export default function CadatrarColaborador() {
         <View>
             <Logo />
             <Text>Cadatrar Colaborador</Text>
-        </View>
+
+            <TextInput
+                label={'Nome do colaborador:'}
+                placeholder={'Digite o nome do colaborador'}
+                onChangeText={text => setNome(text)}
+            />
+
+            <TextInput
+                label={'Email do colaborador:'}
+                placeholder={'Digite seu email'}
+                onChangeText={text => setEmail(text)}
+            />
+            <TextInput
+                label={'Registro:'}
+                placeholder={'Digite seu registro'}
+                onChangeText={text => setRegistro(text)}
+            />
+
+            <TextInput
+                label={'Data de admissão:'}
+                placeholder={'Digite sua data de admissão'}
+                onChangeText={text => setdataAdmissao(text)}
+            />
+
+            <Button onPress={() => console.log("clicou")} title={"Continuar"}>
+            </Button>
+
+        </View >
     );
 };
 
