dd1bed322bd9e7f9bbb5951d835796db243e2850
Andre <andrelucasruizalmeida@gmail.com>
2022-09-27 23:41:28 -0300

    layout: adicionar cor de borda

--- src/globals/colors.js
+++ src/globals/colors.js
@@ -4,6 +4,7 @@ const colors = {
     text: '#E5E5E5',
     background: '#FFF',
     backgroundColor: 'white',
+    borderColor: 'white',
     submitButton: "#373435",
     submitButtonText: 'white',
     success: 'green',
