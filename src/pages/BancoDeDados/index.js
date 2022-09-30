eb35571cb43d2cf9c7228331c09905e286cba182
Andre <andrelucasruizalmeida@gmail.com>
2022-09-29 23:10:21 -0300

    feat: banco com card

--- src/pages/BancoDeDados/index.js
+++ src/pages/BancoDeDados/index.js
@@ -3,15 +3,18 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 import Logo from '../../common/Logo';
+import Card from '../../components/Card';
 
 import styles from './style';
 
 export default function BancoDeDados({ navigation }) {
 
     return (
-        <View>
-            <Text>Banco De Dados</Text>
-            <Logo />
+        <View style={{ flex: 1 }}>
+            <Card />
+            <View style={{ margin: 8, alignItems: "center" }}>
+                <Logo width={146} />
+            </View>
         </View>
     );
 };
